06d4ce90ab8662d325387d9a50ebe2ef3852f053
Glitch (lime-glass) <none>
2018-03-25 20:41:23 +0000

    🏂🌵 Checkpoint ./script.js:789194/2468:789203/244 ./styles.css:789203/8 ./page-one.html:789204/164

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 var progress = 0;
-var text = [];
-function draw(state,choice) {
+var dialogue = [
+  //stage1
+  [
+    "You are an astronaut on a space exploration, Artemis. This spaceship is heading for Gliese 667Cc, a rocky exoplanet, that may inhabit life. You have spent the last 25 years on this spaceship, along with eleven other people. When you arrive by this planet, people of the ship start to point out the moving objects of this planet, life.",
+    "The people controlling this mission tell you to explore it. You and the group have three choices: ",
+    [
+      ["Retreat",],
+      "Explore",
+      "Observe"
+    ],
+  ],
+  //stage2
+  [
+    "You and the people in the spaceship decided to go back to Earth without getting any data on the planet. The moment everyone gets back, the government prohibited space travel and everyone who dares defy the authority is killed.",
+    "prompt",
+    [
+      "choice1",
+      "choice2",
+      "choice3"
+    ],
+  ],
+  [
+    "The people of the spaceship agree with you. They decide to dock the ship on a mountain of the planet. When you and your buddies get off the ship with data-sampling tools, they start collecting information. After taking enough information of the surrounding area, the group is told to explore a different area.",
+    "The group sees two areas that stand out:",
+    [
+      "choice1",
+      "choice2",
+      "choice3"
+    ],
+  ],
+  [
+    "dialogue",
+    "prompt",
+    [
+      "choice1",
+      "choice2",
+      "choice3"
+    ],
+  ],
+  //stage3
+  
+];
+function draw(state) {
   
 };
 
