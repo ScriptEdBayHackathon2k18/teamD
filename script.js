7071ca98dfc61320666132757b47f9aa2f33e580
Glitch (lime-glass) <none>
2018-03-25 20:19:23 +0000

    🎂🕌 Checkpoint ./script.js:789194/1572 ./page-one.html:789194/1603 ./index.html:789194/889:789203/18 ./styles.css:789194/51

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var progress = 0;
-var desc = [
-"On Earth, the human race discovered a technology that allows them to detect habitable planets and go to them in around the speed of light.  Some humans took part in a program, mission Artemis, in which they get paid to go and explore other planets. These humans built a spaceship and journeyed to the “Gliese 667Cc”, which is about 22.18 light years away from Earth. Little did they know, that there is a modern civilization inhabiting the planet, and now, the fate of both the human race and the alien race depends on the hands of the human in that spaceship as they are given choices that have different consequences. "
-
+var text = [
+  "",
 ];
-function {};
+function draw(state) {
+  
+};
 
 
 
